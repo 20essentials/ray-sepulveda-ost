a2e986798154c4d6cd7f9517ce26609f1e181cf5
DominicDev <s0l0estudi4r@gmail.com>
2026-03-13 18:01:14 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RAY SEPÚLVEDA';
+export let nameSongs = [
+	"SI TÚ TE VAS", /*1*/
+	"NO VALE LA PENA", /*2*/
+	"BANDOLERA", /*3*/
+	"MASCARADA", /*4*/
+	"DONDE ESTARÁS", /*5*/
+	"SIN SABER", /*6*/
+	"CUANDO VUELVAS CONMIGO", /*7*/
+	"AHÍ OTRA EN TÚ LUGAR", /*8*/
+	"AHORA ME TOCA A MI", /*9*/
+	"UNA MUJER COMO TÚ", /*10*/
+	"NO SE COMO TE QUIERO", /*11*/
+	"ALGUIEN TE ESTA HABLANDO MAL DE MI", /*12*/
+	"SI NO TENGO TU AMOR", /*13*/
+	"CARIÑO MIO", /*14*/
+	"AHÍ OTRA EN TÚ LUGAR", /*15*/
+	"PELIGROSO AMOR", /*16*/
+	"AHÍ OTRA EN TÚ LUGAR", /*17*/
+	"AQUELLOS TIEMPOS", /*18*/
+	"VOLVERÉ", /*19*/
+	"PARA SIEMPRE", /*20*/
+]
